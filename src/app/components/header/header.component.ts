c33b0b803339b4ac23ac0bd8b0095d7762559c84
Michalis Prodromou <mprodromou@trading-point.com>
2021-09-06 12:25:30 +0300

    created service to swotch between theme modes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ThemeOptions, ThemeSwitcherService} from '../../services/theme-switcher.service';
 
 @Component({
   selector: 'app-header',
@@ -6,14 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public themeOptions = ThemeOptions;
+  public themeMode = ThemeOptions.light;
 
-  constructor() { }
+  constructor(private themeService: ThemeSwitcherService) { }
 
   ngOnInit(): void {
+    this.themeService.themeModeState.subscribe( (state: ThemeOptions) => {
+      this.themeMode = state;
+    });
   }
 
   public toggleTheme() {
-
+    let optionToSet = this.themeMode === ThemeOptions.light ? ThemeOptions.dark : ThemeOptions.light;
+    console.log(optionToSet)
+    this.themeService.setThemeMode(optionToSet);
   }
 
 }
