ae5d175c3bd3c828c05faafca0c71bead8bed91e
Michalis Prodromou <mprodromou@trading-point.com>
2021-09-13 14:18:40 +0300

    fixed theme toggle css. Try to do it with angular animations next?

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ThemeOptions, ThemeSwitcherService} from '../../services/theme-switcher.service';
 
 @Component({
@@ -7,6 +7,8 @@ import {ThemeOptions, ThemeSwitcherService} from '../../services/theme-switcher.
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('themeToggler') themeToggle!:  ElementRef<HTMLElement>;
+
   public themeOptions = ThemeOptions;
   public themeMode = ThemeOptions.light;
 
@@ -16,11 +18,25 @@ export class HeaderComponent implements OnInit {
     this.themeService.themeModeState.subscribe( (state: ThemeOptions) => {
       this.themeMode = state;
     });
+
+    this.setThemePreference();
   }
 
   public toggleTheme() {
     let optionToSet = this.themeMode === ThemeOptions.light ? ThemeOptions.dark : ThemeOptions.light;
+    this.themeToggle.nativeElement.setAttribute('aria-label', 'Switch to ' +  optionToSet + ' theme');
     this.themeService.setThemeMode(optionToSet);
+  }
+
+  public setThemePreference() {
+    console.log('hi')
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      this.themeService.setThemeMode(ThemeOptions.dark);
+      this.themeToggle.nativeElement.setAttribute('aria-label', 'Switch to ' +  ThemeOptions.dark + ' theme');
+      return;
+    }
+    this.themeService.setThemeMode(ThemeOptions.light);
+    this.themeToggle.nativeElement.setAttribute('aria-label', 'Switch to ' +  ThemeOptions.light + ' theme');
   }
 
 }
